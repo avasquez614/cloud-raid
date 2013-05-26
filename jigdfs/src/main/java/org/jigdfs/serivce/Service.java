2655ee34bafb84729d731f4252b7739a992a3b65
Alfonso Vásquez <axeldxter@gmail.com>
2013-05-25 18:32:02 -0600

    Adding JigDFS sub-module.

---
+++ jigdfs/src/main/java/org/jigdfs/serivce/Service.java
@@ -0,0 +1,28 @@
+package org.jigdfs.serivce;
+
+/**
+ * Service base class
+ * @author jbian
+ *
+ */
+public abstract class Service { 
+    
+    public abstract Object getResult();
+    
+    /**
+     * 
+     */
+    protected boolean isRunning = false;
+        
+    protected void setIsRunning(boolean isRunning){
+	this.isRunning = isRunning;
+    }
+    
+    public boolean isRunning() {
+	return isRunning;
+    }
+    /**
+     * run this service
+     */
+    public abstract void runService() throws Exception;
+}
